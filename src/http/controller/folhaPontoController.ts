f4523abc68ac6c9a9626c352abd016ce3234b598
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-08-06 10:43:56 -0300

    feat => configuração da conexão com o banco

--- src/http/controller/folhaPontoController.ts
+++ src/http/controller/folhaPontoController.ts
@@ -1,29 +1,18 @@
 import { Request, Response } from "express";
 import { Horario } from "../interface/horario";
-import fs from 'fs';
-const semana: Horario[] = require('../../../semana.json');
+
 
 class FolhaPontoController {
 
   public async consultaSemana(req: Request, res: Response) {
-    try{
-      return res.send(semana).status(200);
-    }catch{
-      return res.send('Erro arquivo não encontrado.').status(404)
-    }
+
     
   }
 
   public async salvarSemana(req: Request, res: Response) {
-    semana.push(req.body)
 
-    fs.writeFile("semana.json", JSON.stringify(semana), err => {
-      // Checking for errors
-      if (err) throw err; 
 
-    });
 
-    return res.send(semana).status(200);
   }
 
 }
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,3 @@
-import express from 'express';
-import bodyParser from 'body-parser'
-import 'dotenv/config';
-import { apiRouter } from './router/routerController';
+import App from './app'
 
-
-const app: express.Application = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// * Routes
-app.use(apiRouter)
-
-// Porta do servidor
-const PORT = process.env.PORT || 4000;
-
-// Host do servidor
-const HOSTNAME = process.env.HOSTNAME || 'http://localhost';
-
-// inicialização da API
-app.listen(PORT, () => {
-  console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`)
-});
+App.server();
